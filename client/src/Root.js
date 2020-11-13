9edc4baeca661b3d3fd9b8497cb9a2297303d2fe
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-11-13 22:29:42 +0530

    Profile and Register Screen

--- client/src/Root.js
+++ client/src/Root.js
@@ -9,7 +9,14 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 const middlewares = [thunk];
 
 const store = createStore(
