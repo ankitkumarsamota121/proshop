d0dd2a3985132dfa26c4583e7b2e7ce5a55e1da8
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-11-15 17:51:13 +0530

    Order Screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,10 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -18,7 +22,11 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/profile' component={ProfileScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' exact component={HomeScreen} />
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -7,6 +7,12 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './userReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderListMyReducer,
+  orderPayReducer,
+} from './orderReducers';
 
 export default combineReducers({
   productList: productListReducer,
@@ -16,4 +22,8 @@ export default combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
 });
