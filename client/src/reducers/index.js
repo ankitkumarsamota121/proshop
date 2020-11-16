9f143caca2cb7577cff95ed18541c66076ab9dff
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-11-16 16:30:41 +0530

    Update Users and Products Screen

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,20 @@
 import { combineReducers } from 'redux';
-import { productListReducer, productDetailsReducer } from './productReducers';
+import {
+  productListReducer,
+  productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
+} from './productReducers';
 import { cartReducer } from './cartReducers';
 import {
   userLoginReducer,
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userUpdateReducer,
+  userListReducer,
+  userDeleteReducer,
 } from './userReducers';
 import {
   orderCreateReducer,
@@ -17,11 +26,17 @@ import {
 export default combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,13 +3,22 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  getUsers,
   updateUserProfile,
+  deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import { protect, admin } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
