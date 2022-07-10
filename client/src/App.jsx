698c63e5eb8ba6c1dc16db4b94dffbf1c6fe9551
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2022-07-10 13:55:02 +0530

    :sparkles: testing some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// Testing out some changes
-
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
